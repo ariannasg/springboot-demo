18bd5add1a03f17feacff87730e9166eb3bf0517
Ari <ariannasg.ldn@gmail.com>
2020-05-26 12:43:53 +0100

    Added comment

--- src/main/java/com/training/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/training/springbootdemo/SpringbootDemoApplication.java
@@ -4,9 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
+// the @SpringBootApplication is the trigger for Spring Boot Autoconfiguration.
 // using @SpringBootApplication annotation makes this file a configuration file.
 // it also makes this file the starting of all our components scanning.
-// it also initiates  the auto-configuration capabilities that Spring Boot  offers
+// it also initiates  the auto-configuration capabilities that Spring Boot offers
 @SpringBootApplication
 public class SpringbootDemoApplication {
 
